39f817ecfa2cef41292529f02787b6815e2f4aad
John Muller <Johnmuller2000@outlook.com>
2023-06-20 10:37:13 +0200

    add requirements file

--- App/__init__.py
+++ App/__init__.py
@@ -1,8 +1,6 @@
-from dotenv import load_dotenv
 from flask import Flask
 import os
 
-load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from App import app
+from dotenv import load_dotenv
 
+load_dotenv()
+from App import app
 
 if __name__ == "__main__":
     app.run(debug=True)
