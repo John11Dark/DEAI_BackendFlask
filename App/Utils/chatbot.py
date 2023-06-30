b8506b7ad7be852ac73f70fc1b0a50e1e61544a6
John Muller <Johnmuller2000@outlook.com>
2023-06-30 06:31:00 +0200

    fix bugs

--- App/Utils/chatbot.py
+++ App/Utils/chatbot.py
@@ -2,33 +2,25 @@
 
 import os
 from typing import Final
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 import json
 from difflib import get_close_matches
-from . import paths
-from .SentimentAnalyzer import SentimentAnalyzer
+import paths
 
 from typing import TypeAlias
 
 
-# Define custom types using TypeAlias
-Review = TypeAlias(str, bound="Review")
-Response = TypeAlias(str, bound="Response")
-
-
 class Chatbot:
-    GLOBAL_PATH: Final = os.path.join(paths.MODELS_DATA, "/knowledgeBase.json")
+    GLOBAL_PATH: Final = os.path.join(paths.MODELS_DATA, "knowledgeBase.json")
 
     def __init__(
         self,
         knowledge_base_path: str = GLOBAL_PATH,
         similarity_threshold: float = 0.3,
-        type: Review or Response = None,
+        type: str = None,
         question: str = None,
         new_knowledge_base_data: dict = {},
         knowledge_base_data: dict = {},
-        analyzer: SentimentAnalyzer = SentimentAnalyzer(),
+        business_id: str = None,
     ) -> None:
         self.knowledge_base_path = knowledge_base_path
         self.similarity_threshold = similarity_threshold
@@ -36,7 +28,15 @@ class Chatbot:
         self.question = question
         self.new_knowledge_base_data = new_knowledge_base_data
         self.knowledge_base_data = knowledge_base_data
-        self.analyzer = analyzer
+        self.business = self.get_business(business_id)
+
+    @staticmethod
+    def get_business(business_id: str) -> dict:
+        return {
+            "id": business_id,
+            "name": "Business Name",
+            "model_path": "path/to/model",
+        }
 
     def save_knowledge_base(self) -> None:
         with open(self.knowledge_base_path, "w") as file:
@@ -50,26 +50,28 @@ class Chatbot:
         if self.question == "bye":
             print("Bot: Bye! Take care.")
             exit()
-        matches: list = get_close_matches(
-            self.question, self.knowledge_base_data.keys(), n=1, cutoff=0.6
-        )
+        elif self.question == "support":
+            print("Bot: Please wait while a support agent contact you!.")
+            exit()
+        else:
+            matches: list = get_close_matches(
+                self.question, self.knowledge_base_data.keys(), n=1, cutoff=0.6
+            )
         return matches[0] if matches else None
 
     def generate_response(self) -> str:
-        response: str = ""
-        self.load_knowledge_base()
-        if self.analyze_text() in self.knowledge_base_data["questions"]:
-            response = self.knowledge_base_data[self.analyze_text()]
-        else:
-            response = "Sorry, I don't understand your question."
-        return response
+        match: list(str) = self.analyze_text()
+        if match == None:
+            return "Smeorry, I don't understand that."
 
 
 if __name__ == "__main__":
+    chatbot: Chatbot = Chatbot(business_id="1")
+
+    print(
+        f"Bot: Hello! This is an AI chatbot designed for the {chatbot.business.get('name')} experiment. If you need assistance from our support team, please type 'support', and they will respond to you promptly."
+    )
     while True:
-        print(
-            "Bot: Hello, I am a chatbot. I will answer your questions about Chatbots. If you want to exit, type bye!"
-        )
         question: str = input("You: ")
         chatbot: Chatbot = Chatbot(question=question)
         print(f"Bot: {chatbot.generate_response()}")
